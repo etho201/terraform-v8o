9b512f2524ebc8f5efbd6d00846721470ea492e6
Erik Thomsen <ethomsen86@gmail.com>
2022-09-28 12:16:51 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "4.95.0"
+    }
+  }
+}
+
+provider "oci" {
+  # API Key Authentication
+  tenancy_ocid     = "${var.tenancy_ocid}"
+  user_ocid        = "${var.user_ocid}"
+  fingerprint      = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region           = "${var.region}"
+}
+
+# Generate SSH Key Pair for access
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "local_sensitive_file" "ssh_private_key" {
+  filename = "${path.module}/instances/${var.instance_name}/id_rsa"
+  file_permission = "600"
+  content = tls_private_key.ssh.private_key_pem
+}
+
+# Get a list of Availability Domains
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = "${var.compartment_ocid}"
+}
+
+# Create an OCI core instance
+resource "oci_core_instance" "ol_instance" {
+    depends_on = [
+      tls_private_key.ssh
+    ]
+    display_name = var.instance_name
+    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
+    compartment_id = "${var.compartment_ocid}"
+    shape = "VM.Standard.E4.Flex"
+    shape_config {
+      ocpus = 4
+      memory_in_gbs = 48
+    }
+
+    source_details {
+        source_id = "${var.os_image_ocid}"
+        source_type = "image"
+    }
+    create_vnic_details {
+        assign_public_ip = true
+        subnet_id = var.subnet_ocid
+    }
+    metadata = {
+        ssh_authorized_keys = tls_private_key.ssh.public_key_openssh
+    } 
+}
+
+output "connection_details" {
+  value = format("ssh -i ./instances/${var.instance_name}/id_rsa opc@%s", oci_core_instance.ol_instance.public_ip)
+  description = "Details how to connect using the public IP address of the instance."
+}
+
+resource "local_sensitive_file" "readme" {
+  filename = "${path.module}/instances/${var.instance_name}/README.md"
+  content = oci_core_instance.ol_instance.public_ip
+}
