4c44c16e214eccf1959862af5b4dd1fa4e1c5b77
Erik Thomsen <ethomsen86@gmail.com>
2022-09-30 15:01:01 -0400

    Updated formatting

--- variables.tf
+++ variables.tf
@@ -1,46 +1,46 @@
 variable "tenancy_ocid" {
   description = "OCID of your tenancy"
-  type = string
+  type        = string
 }
 
 variable "compartment_ocid" {
   description = "OCID of target compartment within tenancy"
-  type = string
+  type        = string
 }
 
 variable "user_ocid" {
   description = "OCID of the user calling the API"
-  type = string
+  type        = string
 }
 
 variable "fingerprint" {
   description = "Fingerprint for the key pair being used"
-  type = string
+  type        = string
 }
 
 variable "private_key_path" {
   description = "The path to the private key file"
-  type = string
+  type        = string
 }
 
 variable "region" {
   description = "An OCI region"
-  type = string
+  type        = string
 }
 
 variable "subnet_ocid" {
   description = "The OCID of the subnet to create the VNIC in"
-  type = string
+  type        = string
 }
 
 variable "instance_name" {
   description = "A user-friendly name. Does not have to be unique, and it's changeable."
-  type = string
-  default = "My first IaC instance"
+  type        = string
+  default     = "My first IaC instance"
 }
 
 variable "os_image_ocid" {
   description = "The OCID for the OS image."
-  type = string
-  default = "ocid1.image.oc1.iad.aaaaaaaa24ckjg36yh22ksnu7wuzozne6wnh5go4dsbycod244oguv42t6lq"
+  type        = string
+  default     = "ocid1.image.oc1.iad.aaaaaaaa24ckjg36yh22ksnu7wuzozne6wnh5go4dsbycod244oguv42t6lq"
 }
